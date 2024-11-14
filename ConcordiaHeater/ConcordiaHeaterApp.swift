b82297ccf1f7ed832ea282e070bebbdbc5feeebd
Emil Pedersen <emil.pedersen@epitech.eu>
2024-11-14 16:23:23 -0500

    Initial Commit

---
+++ ConcordiaHeater/ConcordiaHeaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcordiaHeaterApp.swift
+//  ConcordiaHeater
+//
+//  Created by Emil Pedersen on 14/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct ConcordiaHeaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ConcordiaHeaterTests/ConcordiaHeaterTests.swift
@@ -0,0 +1,16 @@
+//
+//  ConcordiaHeaterTests.swift
+//  ConcordiaHeaterTests
+//
+//  Created by Emil Pedersen on 14/11/2024.
+//
+
+import Testing
+
+struct ConcordiaHeaterTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
